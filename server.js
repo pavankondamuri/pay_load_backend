27a8952373fd91604799d6ca8e4f2ae3668ff3a0
pavan <pavankonadmuri696@gmail.com>
2025-08-14 20:06:20 +0530

    login

--- server.js
+++ server.js
@@ -12,7 +12,10 @@ dotenv.config();
 const app=express();
 app.use(express.json());
 app.use(cors(
-    
+    {
+        origin:"https://pay-load.vercel.app",
+        credentials:true
+    }
 ));
 const PORT=process.env.PORT || 3000;
 
